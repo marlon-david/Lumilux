c7ebe3791da53169fda05684b7704a5664f91f80
Marlon Oliveira <marlon_dg_oliveira@outlook.com>
2016-07-30 11:13:36 -0300

    Página Loja

--- source/js/sidebar.js
+++ source/js/sidebar.js
@@ -1,28 +1,8 @@
-$('.sidebar span[data-toggle="collapse"]').click(function(){
-	var fa = $(this).find('.fa');
-	var status = fa.attr('class').replace('fa ','');
+$('.menu-lateral [data-toggle="collapse"]').each(function(){
+	var target = $(this).data('target') || this.href;
+	var $target = $(target);
 
-	if(status == 'fa-minus'){
-		fa.removeClass('fa-minus').addClass('fa-plus')
-	}else{
-		fa.removeClass('fa-plus').addClass('fa-minus')
+	if($target.length && !$target.hasClass('in')){
+		$(this).addClass('collapsed');
 	}
-
-	console.log(status)
-});
-
-$('.sidebar .aux').click(function(){
-	$('.sidebar').removeClass('open');
-})
-
-$('.btn-toggle-sidebar').click(function(){
-	$('.sidebar').addClass('open');
-})
-
-$('html').on('swipeleft',function(){
-	$('.sidebar').removeClass('open');
-});
-
-$('html').on('swiperight',function(){
-	$('.sidebar').addClass('open');
 });
